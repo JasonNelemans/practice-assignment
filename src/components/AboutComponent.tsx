2db6c770c1b597a3ffa6d3de75eff7deb9a1f392
JasonNelemans <jasonnelemans@hotmail.com>
2020-09-28 12:42:06 +0200

    created simple AboutComponent

---
+++ src/components/AboutComponent.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+interface Props {
+  content: string;
+}
+
+export default function AboutComponent({ content }: Props) {
+  return (
+    <div>
+      Hello
+    </div>
+  )
+}
