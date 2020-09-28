62ce858de5e1ee89f71fbfa9a0fff3db96fedd1a
JasonNelemans <jasonnelemans@hotmail.com>
2020-09-28 13:31:39 +0200

    styled HotelCard

--- src/components/HotelCard.tsx
+++ src/components/HotelCard.tsx
@@ -10,11 +10,46 @@ interface Props {
 export default function HotelCard({ name, image, price }: Props) {
   return (
     <Container>
-      <img src={image} alt='' />
-      <p>{name}</p>
-      <p>{price}</p>
+      <img src={image} alt='' width='75px' />
+      <Text>
+        <p id='header'>{name}</p>
+        <p id='price'>Vanaf {price},- per persoon</p>
+      </Text>
     </Container>
   )
 }
 
-const Container = styled.div``
+const Container = styled.div`
+  display: flex;
+
+  margin: 15px;
+  margin-left: 0px;
+  width: 375px;
+  height: 75px;
+  border-radius: 3px;
+  box-shadow: 0px 7px 45px 0px rgba(0,0,0,0.1);
+
+  cursor: pointer;
+
+  img {
+    border-radius: 3px 0px 0px 3px;
+  }
+
+  &:hover {
+    opacity: 0.6;
+  }
+`
+
+const Text = styled.div`
+  margin-left: 15px;
+
+  #header {
+    font-weight: bold;
+    margin-bottom: 10px;
+  }
+
+  #price {
+    margin-top: 5px;
+    font-size: 12px;
+  }
+`
