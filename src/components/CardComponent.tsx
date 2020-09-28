f402929495defe1c69c699907c6bc1b78e52d1dc
JasonNelemans <jasonnelemans@hotmail.com>
2020-09-28 11:14:50 +0200

    render data in CardComponent

--- src/components/CardComponent.tsx
+++ src/components/CardComponent.tsx
@@ -24,8 +24,34 @@ type Deal = {
 export default function CardComponent({ deal }: Deal) {
   console.log('deal in Card: ', deal);
   return (
-    <div>
-      This is the card component
-    </div>
+    <CardWrapper>
+      <CardContainer>
+        <div>
+          <img src={deal.image} alt='deal' />
+        </div>
+        <div>
+          <h4>{deal.title}</h4>
+          <p>{deal.description}</p>
+          <p>p.p. vanaf <span><strong>{deal.price / 100},-</strong></span></p>
+        </div>
+      </CardContainer>
+    </CardWrapper>
   )
 }
+
+const CardWrapper = styled.div`
+  width: 280px;
+  height: 260px;
+  border-radius: 3px;
+  box-shadow: 0px 7px 45px 0px rgba(0,0,0,0.1);
+
+  margin-rigt: 20px;
+`
+
+const CardContainer = styled.div`
+  cursor: pointer;
+
+  &:hover {
+    opacity: 0.6;
+  }
+`
