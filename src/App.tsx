2ef1f5ebe56a847a147a04118f0825cd304e69bf
JasonNelemans <jasonnelemans@hotmail.com>
2020-09-28 10:00:19 +0200

    fetch hotel data with cors server

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+
+  async function fetchHotels() {
+    const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
+    const targetUrl = 'https://valkdigital-mock.netlify.com/api/v1/hotels'
+    const res = await fetch(proxyUrl + targetUrl);
+    res
+      .json()
+      .then(({ data }) => console.log('response hotels: ', data))
+      .catch(err => console.log('error hotels: ', err))
+  }
+
+  useEffect(() => {
+    fetchHotels();
+  }, [])
+
   return (
     <div className="App">
     </div>
